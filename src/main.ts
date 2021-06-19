d6d04bb2cc919c91763757224a1fbfd588e3f054
JorgeLNJunior <jorgelnjunior@gmail.com>
2021-06-19 15:56:39 -0300

    docs: adicionar o swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { AppModule } from '@modules/app/app.module';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import { description, name, version } from '../package.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle(name)
+    .setDescription(description)
+    .setVersion(version)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
