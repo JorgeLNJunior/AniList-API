17d4c528ef877b92910c0758769d15b7a1530d72
JorgeLNJunior <jorgelnjunior@gmail.com>
2021-07-01 12:01:46 -0300

    build: remover o dotenv e seus imports

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BcryptService } from '@shared/services/bcrypt.service';
-import { config } from 'dotenv';
 
 import { User } from './entities/user.entity';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-
-config({ path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env' }); // cloudnary
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
