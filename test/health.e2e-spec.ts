8ab658eed7a487d3e97de7cd857603efd3061826
JorgeLNJunior <jorgelnjunior@gmail.com>
2021-07-20 15:15:17 -0300

    test: implementar os testes dos health checks

---
+++ test/health.e2e-spec.ts
@@ -0,0 +1,40 @@
+import { User } from '@modules/user/entities/user.entity';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppModule } from '@src/app.module';
+import * as request from 'supertest';
+import { getRepository } from 'typeorm';
+
+import { AuthHelper } from './helpers/auth.helper';
+
+describe('HealthController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        forbidUnknownValues: true,
+        whitelist: true,
+      }),
+    );
+    await app.init();
+  });
+  afterEach(() => app.close());
+
+  it('/animes (GET) Should return a list of health checks', async () => {
+    const user = await getRepository(User).find({ where: { name: 'admin' } });
+    const token = new AuthHelper(user[0]).sign();
+
+    const { status, body } = await request(app.getHttpServer())
+      .get('/health')
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(status).toBe(200);
+    expect(body).toHaveProperty('status');
+  });
+});
