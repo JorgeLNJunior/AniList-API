31c7ab20f0b7ec44e56f50bcc89c0328d0c6a304
JorgeLNJunior <jorgelnjunior@gmail.com>
2021-08-25 15:26:56 -0300

    refactor: habilitar o cors diretamente pelo nest

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as cors from 'cors';
 import * as express from 'express';
 import * as helmet from 'helmet';
 import { resolve } from 'path';
@@ -13,9 +12,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(helmet());
-  app.use(cors());
   app.use(express.static(resolve('public')));
 
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       forbidUnknownValues: true,
