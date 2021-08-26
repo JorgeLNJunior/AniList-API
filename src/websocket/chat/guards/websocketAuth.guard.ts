c0352f9e4a1b99c24f2ff0667d57fad339d1283c
JorgeLNJunior <jorgelnjunior@gmail.com>
2021-08-26 17:24:15 -0300

    chore: retornar erro no websocket se não estiver autenticado

--- src/websocket/chat/guards/websocketAuth.guard.ts
+++ src/websocket/chat/guards/websocketAuth.guard.ts
@@ -1,17 +1,22 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { WsException } from '@nestjs/websockets';
 
 @Injectable()
 export class WebSocketAuthGuard implements CanActivate {
   constructor(private jwtService: JwtService) {}
 
   async canActivate(context: ExecutionContext) {
-    try {
-      const data = context.switchToWs().getClient();
-      await this.jwtService.verifyAsync(data.handshake.query.auth);
-      return true;
-    } catch (error) {
-      return false;
-    }
+    const data = context.switchToWs().getClient();
+    return this.jwtService
+      .verifyAsync(data.handshake.query.auth)
+      .then(() => {
+        return true;
+      })
+      .catch(() => {
+        throw new WsException(
+          'provide a valid auth token in "auth" query param',
+        );
+      });
   }
 }
