a9cc7b9596eae0c158b2d7298aaa22e3b86cfa13
JorgeLNJunior <jorgelnjunior@gmail.com>
2021-09-04 15:11:09 -0300

    test(unit): testar o is forbidden name decorator

---
+++ src/http/modules/user/__tests__/decorators/isNotForbiddenName.spec.ts
@@ -0,0 +1,29 @@
+import { IsNotForbiddenNameConstraint } from '../../decorators/isNotForbiddenName.decorator';
+
+describe('IsNotForbiddenNameDecorator', () => {
+  let decorator: IsNotForbiddenNameConstraint;
+
+  beforeEach(() => {
+    decorator = new IsNotForbiddenNameConstraint();
+  });
+
+  test('should return false if has received "admin" in lowercase', async () => {
+    expect(decorator.validate('admin')).toBe(false);
+  });
+
+  test('should return false if has received "admin" in uppercase', async () => {
+    expect(decorator.validate('ADMIN')).toBe(false);
+  });
+
+  test('should return false if has received "admin" in uppercase and lowercase', async () => {
+    expect(decorator.validate('AdMiN')).toBe(false);
+  });
+
+  test('should return true if not received "admin"', async () => {
+    expect(decorator.validate('user')).toBe(true);
+  });
+
+  test('should return a default message', async () => {
+    expect(decorator.defaultMessage()).toBe('forbidden "name"');
+  });
+});
