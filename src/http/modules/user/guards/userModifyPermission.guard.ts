c22b4747fe663796055031c41eb32e4ebc91fc5f
JorgeLNJunior <jorgelnjunior@gmail.com>
2021-09-04 14:45:23 -0300

    chore: remover tipo de retorno não usado

--- src/http/modules/user/guards/userModifyPermission.guard.ts
+++ src/http/modules/user/guards/userModifyPermission.guard.ts
@@ -1,10 +1,7 @@
 import { CanActivate, ExecutionContext } from '@nestjs/common';
-import { Observable } from 'rxjs';
 
 export class UserModifyPermissionGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest();
     if (request.user.isAdmin) return true;
 
