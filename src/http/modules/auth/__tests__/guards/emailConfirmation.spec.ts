a86b234fc777bfec52212e999b97aaef65724aec
JorgeLNJunior <jorgelnjunior@gmail.com>
2021-09-08 16:19:18 -0300

    test(unit): testar o is email confirmed guard

---
+++ src/http/modules/auth/__tests__/guards/emailConfirmation.spec.ts
@@ -0,0 +1,40 @@
+import { createMock } from '@golevelup/ts-jest';
+import { ExecutionContext, UnauthorizedException } from '@nestjs/common';
+
+import { EmailConfirmationGuard } from '../../guard/emailConfirmation.guard';
+
+describe('EmailConfirmationGuard', () => {
+  let guard: EmailConfirmationGuard;
+
+  beforeEach(() => {
+    guard = new EmailConfirmationGuard();
+  });
+
+  test('should return true if the email is already confirmed', async () => {
+    const ctx = createMock<ExecutionContext>({
+      switchToHttp: () => ({
+        getRequest: () => ({
+          user: { isEmailConfirmed: true },
+        }),
+      }),
+    });
+
+    const result = guard.canActivate(ctx);
+
+    expect(result).toBe(true);
+  });
+
+  test('should return throw a UnauthorizedException if the email is not confirmed', async () => {
+    const ctx = createMock<ExecutionContext>({
+      switchToHttp: () => ({
+        getRequest: () => ({
+          user: { isEmailConfirmed: false },
+        }),
+      }),
+    });
+
+    expect(() => {
+      guard.canActivate(ctx);
+    }).toThrow(UnauthorizedException);
+  });
+});
