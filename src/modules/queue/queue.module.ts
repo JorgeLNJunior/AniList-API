fe71d35532fc93f808b91d1970654a748dc17443
JorgeLNJunior <jorgelnjunior@gmail.com>
2021-09-11 13:49:33 -0300

    fix: importar os módulos necessários no email module

--- src/modules/queue/queue.module.ts
+++ src/modules/queue/queue.module.ts
@@ -3,9 +3,13 @@ import { User } from '@http/modules/user/entities/user.entity';
 import { MailService } from '@http/shared/services/mail/mail.service';
 import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AnimeStorage } from '@src/http/modules/anime/storage/anime.storage';
 import { UserStorage } from '@src/http/modules/user/storage/user.storage';
+import { FakeMailService } from '@src/http/shared/services/mail/fakeMail.service';
+import { SendgridMailService } from '@src/http/shared/services/mail/sendgridMail.service';
 
 import { AvatarCompressConsumer } from './consumers/avatar.consumer';
 import { CoverCompressConsumer } from './consumers/cover.consumer';
@@ -13,6 +17,7 @@ import { EmailConsumer } from './consumers/email.consumer';
 
 @Module({
   imports: [
+    JwtModule.register({}),
     TypeOrmModule.forFeature([Anime, User]),
     BullModule.registerQueueAsync(
       { name: 'cover-compression' },
@@ -21,10 +26,13 @@ import { EmailConsumer } from './consumers/email.consumer';
     ),
   ],
   providers: [
+    ConfigService,
     CoverCompressConsumer,
     AvatarCompressConsumer,
     EmailConsumer,
     MailService,
+    FakeMailService,
+    SendgridMailService,
     AnimeStorage,
     UserStorage,
   ],
