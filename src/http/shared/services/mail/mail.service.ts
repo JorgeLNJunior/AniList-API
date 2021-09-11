f13863771aa1964bd02df157e239402b4ec811db
JorgeLNJunior <jorgelnjunior@gmail.com>
2021-09-11 12:39:30 -0300

    refactor: injetar as dependências do mail service

--- src/http/shared/services/mail/mail.service.ts
+++ src/http/shared/services/mail/mail.service.ts
@@ -1,8 +1,6 @@
 import { User } from '@http/modules/user/entities/user.entity';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { JwtService } from '@nestjs/jwt';
-import { getRepository } from 'typeorm';
 
 import { FakeMailService } from './fakeMail.service';
 import { IMailService } from './interface/mail.service.interface';
@@ -10,20 +8,24 @@ import { SendgridMailService } from './sendgridMail.service';
 
 @Injectable()
 export class MailService implements IMailService {
-  constructor(private configService: ConfigService) {}
+  constructor(
+    private configService: ConfigService,
+    private sendgridMailService: SendgridMailService,
+    private fakeMailService: FakeMailService,
+  ) {}
 
   async sendConfirmationEmail(user: User): Promise<void> {
     await this.getMailService().sendConfirmationEmail(user);
   }
 
-  getMailService() {
+  getMailService(): IMailService {
     const env = this.configService.get<string>('MAIL_SERVICE').toLowerCase();
 
     switch (env) {
       case 'sendgrid':
-        return new SendgridMailService(new JwtService({}), new ConfigService());
+        return this.sendgridMailService;
       case 'fake':
-        return new FakeMailService(getRepository(User));
+        return this.fakeMailService;
       default:
         throw new Error(`"${env}" is a invalid mail service`);
     }
