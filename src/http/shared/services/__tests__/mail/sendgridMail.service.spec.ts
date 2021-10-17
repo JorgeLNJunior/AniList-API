c39f5033fd551b553fa9d7ad8cbad5eaede7dfe1
JorgeLNJunior <jorgelnjunior@gmail.com>
2021-10-17 11:42:52 -0300

    test(unit): testar o sendgrid mail service

---
+++ src/http/shared/services/__tests__/mail/sendgridMail.service.spec.ts
@@ -0,0 +1,44 @@
+import { fakeUser } from "@mocks/fakes";
+import { ConfigService } from "@nestjs/config";
+import { JwtModule } from "@nestjs/jwt";
+import { Test } from "@nestjs/testing";
+
+import { SendgridMailService } from "../../mail/sendgridMail.service";
+
+jest.mock('@sendgrid/mail', () => ({
+  setApiKey: jest.fn(),
+  send: jest.fn()
+}))
+
+// eslint-disable-next-line import/first
+import * as sgMail from '@sendgrid/mail'
+
+describe('SendgridMailService', () => {
+  let service: SendgridMailService
+  let config: ConfigService
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      imports: [JwtModule.register({})],
+      providers: [
+        SendgridMailService,
+        ConfigService
+      ]
+    }).compile()
+
+    service = module.get(SendgridMailService)
+    config = module.get(ConfigService)
+  })
+
+  afterEach(() => jest.clearAllMocks())
+
+  test('should send a email', async () => {
+    jest.spyOn(config, 'get').mockReturnValue('1d')
+
+    await service.sendConfirmationEmail(fakeUser)
+
+    expect(config.get).toBeCalled()
+    expect(sgMail.setApiKey).toBeCalledTimes(1)
+    expect(sgMail.send).toBeCalledTimes(1)
+  });
+});
