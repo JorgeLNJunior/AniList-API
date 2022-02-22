1b968767c571e74ecc480ea9eec471f269eb1805
JorgeLNJunior <jorgelnjunior@gmail.com>
2022-02-22 14:27:05 -0300

    test(unit): testar o http service

---
+++ src/http/shared/services/__tests__/http.service.spec.ts
@@ -0,0 +1,17 @@
+import { HttpService } from "../http.service";
+
+describe('HttpService', () => {
+  let httpService: HttpService
+
+  beforeEach(() => {
+    httpService = new HttpService()
+  })
+
+  afterEach(() => jest.clearAllMocks())
+
+  test('should return a valid response', async () => {
+    const response = await httpService.get('https://catfact.ninja/fact')
+
+    expect(response.data).toBeDefined()
+  });
+});
