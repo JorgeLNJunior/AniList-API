18648f6b43ce3adc8ebe80f493c07b2cc0c7d8a7
JorgeLNJunior <jorgelnjunior@gmail.com>
2022-04-29 15:25:03 -0300

    test(unit): testar o decorator isValidAnimeUUID

---
+++ src/app/http/modules/anime/__tests__/decorators/isValidAnimeUUID.decorator.spec.ts
@@ -0,0 +1,58 @@
+import { IsValidAnimeUUIDConstraint } from "@http/modules/anime/decorators/isValidAnimeUUID.decorator";
+import { animeRepositoryMock } from "@mocks/repositories/anime.respository.mock";
+import { Test } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+
+import { Anime } from "../../entities/anime.entity";
+
+describe('IsValidAnimeUUIDConstraint', () => {
+  let decorator: IsValidAnimeUUIDConstraint;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        IsValidAnimeUUIDConstraint,
+        {
+          provide: getRepositoryToken(Anime),
+          useValue: animeRepositoryMock
+        }
+      ]
+    }).compile()
+
+    decorator = module.get(IsValidAnimeUUIDConstraint)
+  })
+  afterEach(() => jest.clearAllMocks())
+
+  describe('validate', () => {
+    afterEach(() => jest.clearAllMocks())
+
+    test('should return true if it receives a valid anime uuid', async () => {
+      const uuid = 'uuid'
+
+      const result = await decorator.validate(uuid)
+
+      expect(result).toBe(true)
+    });
+
+    test('should return false if it receives an invalid anime uuid', async () => {
+      const uuid = 'uuid'
+
+      animeRepositoryMock.findOne.mockResolvedValue(undefined)
+
+      const result = await decorator.validate(uuid)
+
+      expect(result).toBe(false)
+    });
+  });
+
+  describe('defaultMessage', () => {
+    afterEach(() => jest.clearAllMocks())
+
+    test('should return the default message', async () => {
+      const message = decorator.defaultMessage()
+
+      expect(message).toBe('this anime does not exist')
+    });
+  });
+
+});
