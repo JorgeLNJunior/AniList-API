e7eb8d5f41f49b3c20e83c7361d5b7de268356ad
JorgeLNJunior <jorgelnjunior@gmail.com>
2022-07-02 19:03:37 -0300

    chore: rodar a aplicação em cluster

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,9 @@ module.exports = {
     {
       name: 'anilist-api',
       script: 'dist/src/main.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      instance_var: 'INSTANCE_ID',
       env: {
         NODE_ENV: 'development',
       },
