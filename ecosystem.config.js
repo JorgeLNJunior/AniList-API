4a3831544e5843b2d60b8bc3d9af2b346d9fe1c0
JorgeLNJunior <jorgelnjunior@gmail.com>
2022-07-02 19:39:54 -0300

    revert: rodar a aplicação em cluster

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,6 @@ module.exports = {
     {
       name: 'anilist-api',
       script: 'dist/src/main.js',
-      instances: 'max',
-      exec_mode: 'cluster',
-      instance_var: 'INSTANCE_ID',
       env: {
         NODE_ENV: 'development',
       },
